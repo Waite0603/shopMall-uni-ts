af0815fc094afbcb6a1725dc60b4f933a280b3a4
Waite Wang <waite@waite.wang>
2024-04-05 15:56:07 +0800

    ✨ feat: add/ finish category.vue

--- src/components/XtxSwiper.vue
+++ src/components/XtxSwiper.vue
@@ -1,6 +1,6 @@
 <template>
   <view class="carousel">
-    <swiper :circular="true" :autoplay="true" :interval="3000" @change="swiperChange">
+    <swiper :circular="true" :autoplay="true" :interval="3000" @change="swiperChange" style="height: 100%;">
       <swiper-item v-for="item in list" :key="item.id">
         <navigator :url="item.hrefUrl" hover-class="none" class="navigator">
           <image mode="aspectFill" class="image" :src="item.imgUrl"></image>
@@ -68,7 +68,7 @@ defineProps<{
   .navigator,
   .image {
     width: 100%;
-    height: 280rpx;
+    height: 100%
   }
 }
 </style>
--- src/pages/category/category.vue
+++ src/pages/category/category.vue
@@ -1,17 +1,29 @@
 <template>
-  <view class="viewport">
+  <page-skeleton v-if="showSkeleton"></page-skeleton>
+  
+  <view class="viewport" v-else>
     <!-- 搜索框 -->
     <view class="search">
       <view class="input">
-        <text class="icon-search">女靴</text>
+        <view>
+          <uni-icons type="search" />
+          adfadf
+        </view>
+
       </view>
     </view>
     <!-- 分类 -->
     <view class="categories">
       <!-- 左侧：一级分类 -->
-      <scroll-view class="primary" scroll-y>
-        <view v-for="(item, index) in 10" :key="item" class="item" :class="{ active: index === 0 }">
-          <text class="name"> 居家 </text>
+      <scroll-view class="primary" scroll-y >
+        <view
+          v-for="(item, index) in categoryTopList"
+          :key="index"
+          class="item"
+          :class="{ active: activeIndex === index }"
+          @click="activeIndex = index"
+        >
+          <text class="name"> {{ item.name }} </text>
         </view>
       </scroll-view>
       <!-- 右侧：二级分类 -->
@@ -19,27 +31,27 @@
         <!-- 焦点图 -->
         <XtxSwiper class="banner" :list="bannerList" />
         <!-- 内容区域 -->
-        <view class="panel" v-for="item in 3" :key="item">
+        <view class="panel" v-for="item in secondaryData" :key="item.id">
           <view class="title">
-            <text class="name">宠物用品</text>
+            <text class="name">{{ item.name }}</text>
             <navigator class="more" hover-class="none">全部</navigator>
           </view>
           <view class="section">
             <navigator
-              v-for="goods in 4"
-              :key="goods"
+              v-for="goods in item.goods"
+              :key="goods.id"
               class="goods"
               hover-class="none"
-              :url="`/pages/goods/goods?id=`"
+              :url="`/pages/goods/goods?id=${goods.id}`"
             >
               <image
                 class="image"
-                src="https://yanxuan-item.nosdn.127.net/674ec7a88de58a026304983dd049ea69.jpg"
+                :src="goods.picture"
               ></image>
-              <view class="name ellipsis">木天蓼逗猫棍</view>
+              <view class="name ellipsis">{{ goods.name }}</view>
               <view class="price">
                 <text class="symbol">¥</text>
-                <text class="number">16.00</text>
+                <text class="number">{{ goods.price }}</text>
               </view>
             </navigator>
           </view>
@@ -51,13 +63,19 @@
 
 <script setup lang="ts">
 import { onLoad } from '@dcloudio/uni-app'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
+
+import PageSkeleton from './components/PageSkeleton.vue'
 
 import { getHomeBannerAPI } from '@/api/home'
 import { getCategoryTopAPI } from '@/api/category'
 import type { BannerItem } from '@/types/home'
+import type { CategoryTopItem } from '@/types/category'
 
 const bannerList = ref<BannerItem[]>([])
+const categoryTopList = ref<CategoryTopItem[]>([])
+const showSkeleton = ref(true)
+const activeIndex = ref(0)
 
 // 获取首页轮播图数据
 const getHomeBannerData = async () => {
@@ -65,8 +83,27 @@ const getHomeBannerData = async () => {
   bannerList.value = res.result
 }
 
+// 获取分类数据
+const getCategoryTopData = async () => {
+  const res = await getCategoryTopAPI()
+  categoryTopList.value = res.result
+}
+
+// 提取二级分类数据
+const secondaryData = computed(() => {
+  if (categoryTopList.value.length) {
+    return categoryTopList.value[activeIndex.value].children
+  }
+  return []
+})
+
 onLoad(() => {
-  getHomeBannerData()
+  Promise.all([
+    getHomeBannerData(),
+    getCategoryTopData()
+  ]).then(() => {
+    showSkeleton.value = false
+  })
 })
 </script>
 
@@ -81,6 +118,7 @@ page {
   flex-direction: column;
 }
 .search {
+  margin-top: 20rpx;
   padding: 0 30rpx 20rpx;
   background-color: #fff;
   .input {
@@ -93,11 +131,6 @@ page {
     font-size: 28rpx;
     border-radius: 32rpx;
     background-color: #f3f4f4;
-  }
-}
-.icon-search {
-  &::before {
-    margin-right: 10rpx;
   }
 }
 /* 分类 */
@@ -195,6 +228,12 @@ page {
         padding: 5rpx;
         font-size: 22rpx;
         color: #333;
+      }
+      .ellipsis {
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 2;
+        overflow: hidden;
       }
       .price {
         padding: 5rpx;
